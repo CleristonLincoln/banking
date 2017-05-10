9519d09c744b7ab9e94a93673404d4351ca2e5ac
CleristonLincoln <cleristonlincoln@hotmail.com>
2017-05-10 17:25:24 -0300

    update

---
+++ src/main/resources/static/js/cliente.js
@@ -0,0 +1,8 @@
+//$('#ClienteRapido').on('shown.bs.modal', function () {
+//  $('#modalClienteRapido').focus();
+//  $('submit_form').click(function(){
+//	  $('#modalClienteRapido').submit();
+//  })
+//})
+
+$('#modalCadastroRapido').modal(options)
